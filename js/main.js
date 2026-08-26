bc7f73c5ac30d00e6621a7f6997d428032bc408e
MCWatts <mcwatts28@proton.me>
2026-08-25 20:07:46 -0700

    modified:   css/ema_gallery.css 	modified:   ema_gallery.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
 let slideNum = 1;
+var timer = null;
 const homeSlide = document.getElementById("home-slide");
 const achromSlide = document.getElementById("achrom-slide");
 const colorSlide = document.getElementById("color-slide");
 const body = document.querySelector("body");
 
-if (body.classList.contains("home")) {
-    dispSlide(slideNum, homeSlide);
+if (body.classList.contains("home")) { // Error prevention on other pages
+    dispSlide(slideNum, homeSlide); // Calls first slide to be displayed on home page
 }
 
 function openModal(slider) { // Toggles the modal divs display "on"
     slider.style.display = "block";
+    document.getElementById("achrom-thumb").scrollLeft = 0;
 }
 
 function closeModal(slider) { // Toggles the modal divs display "off"
     slider.style.display = "none";
 }
 
-function changeSlide(slide, slider) { // Adds and "subtracts" value from change slide function and sets displayed slide to new value
-    dispSlide(slideNum += slide, slider);
+function changeSlide(slide, slider) { // Function to move slides and moves thumbnail slider accordingly
+    slideNum += slide; // Adds or "subtracts" 1 from slide count
+
+    if (slider !== homeSlide) { // Verifies carousel is in gallery 
+        if (slide > 0 && slideNum <= (slider.getElementsByClassName("slides").length)) { // Checks if movement is positive and slide is less than or equal to max
+            document.getElementById("achrom-thumb").scrollLeft += 50; // Moves thumbnail slideshow +50 pixels
+        } else if (slide < 0 && slideNum > 0) { // Checks if movement is negative and slide is greater than minimum
+            document.getElementById("achrom-thumb").scrollLeft -= 50; // Moves thumbnail -50 pixels
+        } else if (slide < 0 && slideNum === 0) { // Checks if movement is negative and slide is at min
+            document.getElementById("achrom-thumb").scrollLeft = document.getElementById("achrom-thumb").scrollWidth; // Moves thumbnail to end
+        } else {
+            document.getElementById("achrom-thumb").scrollLeft -= document.getElementById("achrom-thumb").scrollWidth; // Moves thumbnail to start
+        }
+    }
+
+    clearTimeout(timer);
+    dispSlide(slideNum, slider);
 }
 
 function currSlide(slide, slider) { // Sets displayed slide to value sent to current slide function
+    clearTimeout(timer);
     dispSlide(slideNum = slide, slider);
 }
 
@@ -31,7 +49,7 @@ function dispSlide(slide, slider) {
     const achromCaption = document.getElementById("achrom-caption");
     const colorCaption = document.getElementById("color-caption");
 
-    if (slide === undefined) { // Prevents slide from being undefined on automated loop and sets/resets slide interval
+    if (slide === undefined) { // Uses undefined parameter to ensure automated looping
         slide = ++slideNum;
     }
 
@@ -59,9 +77,13 @@ function dispSlide(slide, slider) {
     
     dots[slideNum - 1].className += " active"; // Adds " active" to end of currently selected dot
 
-    if (slider !== homeSlide) { // Matches alt text to caption for images in lightbox
+    if (slider !== homeSlide) { // Matches alt text to caption for images in lightbox and auto-rotates home carousel
         achromCaption.innerHTML = dots[slideNum - 1].alt;
         colorCaption.innerHTML = dots[slideNum - 1].alt;
+    } else {
+        timer = setTimeout(() => {
+            dispSlide(undefined, homeSlide);
+        }, 6000);
     }
 }
 
@@ -74,7 +96,7 @@ if (body.classList.contains("contact")) {
             return;
         } else {
             e.preventDefault();
-            window.location.href = "/ema_thanks.html";
+            window.location.href = "/Elle-M-Art/ema_thanks.html";
         }
     });
 }
